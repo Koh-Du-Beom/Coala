f8c09b96ec2732cb711ee834730067c81fa39aad
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-03-21 23:40:40 +0900

    code error fix

--- RandomDefense/Week1/Intermediate/11568.js
+++ RandomDefense/Week1/Intermediate/11568.js
@@ -1,8 +1,8 @@
 
 function solution(n, arr) {
-  let dp = [1] * n;
+  let dp = new Array(n).fill(1);
 	for (let i=0; i<n; i++){
-		for (let j=0; j<=i; i++){
+		for (let j=0; j<=i; j++){
 			if(arr[i] > arr[j]){
 				dp[i] = Math.max(dp[i], dp[j] + 1);
 			}
